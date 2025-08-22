a177267484f11f0a3a4ea72331c90918cd9f1d43
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2025-08-22 13:50:09 +0800

    06: categories finalization

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -3,16 +3,19 @@ import React from "react";
 import SearchInput from "./search-input";
 import Categories from "./categories";
 
+import { CustomCategory } from "../types";
+
 interface Props {
-  data: any;
+  data: CustomCategory[];
 }
 
 export const SearchFilters = ({ data }: Props) => {
   return (
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
-      <SearchInput />
-      <Categories data={data} />
-      Search Filters
+      <SearchInput data={data} />
+      <div className="hidden lg:block">
+        <Categories data={data} />
+      </div>
     </div>
   );
 };
