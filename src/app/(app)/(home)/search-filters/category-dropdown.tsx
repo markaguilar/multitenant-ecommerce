cfff529266d9b256885aa98f7aee841250b461b0
Mark Ferdinan Aguilar <markferdinan.mfa@gmail.com>
2025-08-22 16:58:40 +0800

    06: categories finalization

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -58,8 +58,13 @@ const CategoryDropdown = ({
             isOpen &&
               "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]",
           )}
+          asChild
         >
-          <Link href={`/${category.slug === "all" ? "" : category.slug}`}>
+          <Link
+            href={`/${category.slug === "all" ? "" : category.slug}`}
+            prefetch={false}
+            aria-label={`${category.name} category`}
+          >
             {category.name}
           </Link>
         </Button>
